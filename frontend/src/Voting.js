c004696fcf3fec28fad44330fa26523454aacbe1
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-18 13:40:51 -0500

    Remove axios to avoid warning

--- frontend/src/Voting.js
+++ frontend/src/Voting.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase client using environment variables (safe for pushing)
